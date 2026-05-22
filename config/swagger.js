a7ac440e1eb4855f09ebac4eee61bd2209a32e40
Takunda Gowa <takundagowa@gmail.com>
2026-05-22 10:08:49 +0200

    Update Swagger server URL to use deployed host by default

--- config/swagger.js
+++ config/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.API_BASE_URL || `http://localhost:${process.env.PORT || 5000}`,
+        url: process.env.API_BASE_URL || '/',
         description: 'Active server',
       },
       {
